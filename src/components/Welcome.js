5974c8eb4d633beddb386f254fd0afa9c979d445
Максим М <givenchannel2@gmail.com>
2023-12-10 19:21:05 +0200

    some features to be added

---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import "../styles/ToolsAndAdvs.css"
+
+export default function ToolsAndAdvs() {
+
+
+
+    return(
+        <div>
+            <h1>Welcome to our shop!</h1>
+        </div>
+    )
+}
